0b68c52b384e66a90a08885c8f0884d3d8541aca
venkatesh reddy <venkateshreddy623@gmail.com>
2017-12-27 22:31:44 +0530

    this is my third commit

--- src/main/java/ECOMMERS/App.java
+++ src/main/java/ECOMMERS/App.java
@@ -1,13 +1,13 @@
 package ebay;
 
 /**
- * Hello  world!
+ * Hello world!
  *
  */
 public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello  world!" );
+        System.out.println( "Hello world!" );
     }
 }
