e2de5f37ee47d067b1ca826ba6a70026c56ac3f8
venkatesh reddy <venkateshreddy623@gmail.com>
2018-01-10 23:41:23 +0530

    change java file name

--- src/main/java/ECOMMERS/App.java
+++ src/main/java/ECOMMERS/App.java
@@ -12,14 +12,13 @@ public class App
 {
     public static void main( String[] args )
     {
-<<<<<<< HEAD
+
         System.out.println( "Hello world!" );
-=======
-<<<<<<< HEAD
+
+
         System.out.println( "Hello world!" );
-=======
+
         System.out.println( "Hello java world!" );
->>>>>>> a4ada830108487d1b66f46b352f96f93f3ebcb84
->>>>>>> 90b0087f5aa88820787705973d7ce450699b5d83
+		
     }
 }
