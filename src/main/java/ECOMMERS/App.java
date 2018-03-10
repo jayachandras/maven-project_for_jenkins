2bc846cac5fb542d9bc362f9cb975e3ffe3790f0
venkatesh reddy <venkateshreddy623@gmail.com>
2018-03-10 16:22:30 +0530

    add some code

--- src/main/java/ECOMMERS/App.java
+++ src/main/java/ECOMMERS/App.java
@@ -1,13 +1,5 @@
 package ebay;
 
-/**
-<<<<<<< HEAD
- * Hello world!
-=======
- * Hello java world!
->>>>>>> 90b0087f5aa88820787705973d7ce450699b5d83
- *
- */
 public class App
 {
     public static void main( String[] args )
